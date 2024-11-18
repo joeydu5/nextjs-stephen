9195ff48a7bdf6d1c81d04f13d2d2f68342af8ae
Joe Du <joeys.du@gmail.com>
2024-11-18 11:50:25 +1100

    useActionState implement error message and ispending

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -20,3 +20,45 @@ export const deleteSnippet = async (id: number) => {
 
   redirect("/");
 };
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  // // this needs to be a server ction
+  // "use server";
+  try {
+    // check the user's inputs and make sure they are valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+    if (typeof title !== "string" || title.length < 3) {
+      return { message: "title should be longer!" };
+    }
+
+    if (typeof code !== "string" || code.length < 5) {
+      return { message: "code should be longer!" };
+    }
+
+    // throw Error("Failed to save to Database.");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log("snippet", snippet);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return {
+        message: "something is wrong, please check!",
+      };
+    }
+  }
+  // redirect user back to the root route
+  redirect("/");
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,30 +1,14 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
-async function createSnippet(formData: FormData) {
-  // this needs to be a server ction
-  "use server";
-
-  // check the user's inputs and make sure they are valid
-  const title = formData.get("title") as string;
-  const code = formData.get("code") as string;
-
-  // create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-  console.log("snippet", snippet);
-
-  // redirect user back to the root route
-  redirect("/");
-}
+"use client";
+import { createSnippet } from "@/actions";
+import { useActionState } from "react";
 
 function SnippetCreatePage() {
+  const [state, action, isPending] = useActionState(createSnippet, {
+    message: "",
+  });
   return (
     <div>
-      <form action={createSnippet}>
+      <form action={action}>
         <h3 className="font-bold m-3">Create a Snippet</h3>
         <div className="flex flex-col gap-4">
           <div className="flex gap-4">
@@ -48,9 +32,18 @@ function SnippetCreatePage() {
               className="border rounded p-2 w-full"
             />
           </div>
-          <button type="submit" className="rounded p-2 bg-blue-200">
+          <button
+            disabled={isPending}
+            type="submit"
+            className="rounded p-2 bg-blue-200"
+          >
             Create
           </button>
+          {state.message ? (
+            <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+              {state.message}
+            </div>
+          ) : null}
         </div>
       </form>
     </div>
